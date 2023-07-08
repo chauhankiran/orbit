2d63b3485b1f4160d5a4df8275aa8a83a6e05daa
Kiran <kiranchauhan68@gmail.com>
2023-07-08 16:32:48 +0530

    added dummy APIs for adding entities

--- back-end/app.js
+++ back-end/app.js
@@ -8,13 +8,14 @@ const app = express();
 const db = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "Admin@1234",
   database: "orbit",
   connectionLimit: 5,
 });
 
 app.use(morgan("tiny"));
 app.use(cors({ origin: "http://localhost:3000" }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "hello, world" });
@@ -31,12 +32,36 @@ app.get("/companies", async (req, res, next) => {
   }
 });
 
+app.post("/companies", async (req, res, next) => {
+  const { name } = req.body;
+
+  try {
+    const [result] = await db.query("INSERT INTO companies (name) VALUES (?)", [name]);
+
+    res.json({ data: result.insertId });
+  } catch (err) {
+    next(err);
+  }
+});
+
 // Contacts
 app.get("/contacts", async (req, res, next) => {
   try {
     const [contacts] = await db.query("SELECT * FROM contacts");
 
     res.json({ data: contacts });
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post("/contacts", async (req, res, next) => {
+  const { firstName } = req.body;
+
+  try {
+    const [result] = await db.query("INSERT INTO contacts (firstName) VALUES (?)", [firstName]);
+
+    res.json({ data: result.insertId });
   } catch (err) {
     next(err);
   }
@@ -53,12 +78,36 @@ app.get("/deals", async (req, res, next) => {
   }
 });
 
+app.post("/deals", async (req, res, next) => {
+  const { name } = req.body;
+
+  try {
+    const [result] = await db.query("INSERT INTO deals (name) VALUES (?)", [name]);
+
+    res.json({ data: result.insertId });
+  } catch (err) {
+    next(err);
+  }
+});
+
 // Quotes
 app.get("/quotes", async (req, res, next) => {
   try {
     const [quotes] = await db.query("SELECT * FROM quotes");
 
     res.json({ data: quotes });
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post("/quotes", async (req, res, next) => {
+  const { name } = req.body;
+
+  try {
+    const [result] = await db.query("INSERT INTO quotes (name) VALUES (?)", [name]);
+
+    res.json({ data: result.insertId });
   } catch (err) {
     next(err);
   }
@@ -75,12 +124,36 @@ app.get("/tickets", async (req, res, next) => {
   }
 });
 
+app.post("/tickets", async (req, res, next) => {
+  const { name } = req.body;
+
+  try {
+    const [result] = await db.query("INSERT INTO tickets (name) VALUES (?)", [name]);
+
+    res.json({ data: result.insertId });
+  } catch (err) {
+    next(err);
+  }
+});
+
 // Tasks
 app.get("/tasks", async (req, res, next) => {
   try {
     const [tasks] = await db.query("SELECT * FROM tasks");
 
     res.json({ data: tasks });
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post("/tasks", async (req, res, next) => {
+  const { name } = req.body;
+
+  try {
+    const [result] = await db.query("INSERT INTO tasks (name) VALUES (?)", [name]);
+
+    res.json({ data: result.insertId });
   } catch (err) {
     next(err);
   }
